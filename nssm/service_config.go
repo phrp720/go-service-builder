1c784f66b979f013328daf58a8f88fc7737f156a
phrp <filippospapadakis1@gmail.com>
2024-10-28 17:40:14 +0100

    doc update

--- nssm/service_config.go
+++ nssm/service_config.go
@@ -1,12 +1,13 @@
 package nssm
 
 // ServiceConfig basic available  values where found here: https://gist.github.com/magnetikonline/2217fd95cf15a0324696
-// ServiceConfig holds the configuration for an NSSM service
+// ServiceConfig holds the configuration for an NSSM service and the service name
 type ServiceConfig struct {
 	serviceName string
 	parameters  Parameters
 }
 
+// Parameters holds the configuration options for an NSSM service
 type Parameters struct {
 	AppAffinity                  string
 	AppDirectory                 string
