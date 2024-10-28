5976c6848d921852011aab442ff6222a18ac59e8
phrp <filippospapadakis1@gmail.com>
2024-10-28 16:58:50 +0100

    nssm structure implementation

---
+++ nssm/service_config.go
@@ -0,0 +1,52 @@
+package nssm
+
+// ServiceConfig basic available  values where found here: https://gist.github.com/magnetikonline/2217fd95cf15a0324696
+// ServiceConfig holds the configuration for an NSSM service
+type ServiceConfig struct {
+	serviceName string
+	parameters  Parameters
+}
+
+type Parameters struct {
+	AppAffinity                  string
+	AppDirectory                 string
+	AppEnvironment               string
+	AppEnvironmentExtra          string
+	AppExit                      string
+	Application                  string
+	AppNoConsole                 string
+	AppParameters                string
+	AppPriority                  string
+	AppRestartDelay              string
+	AppRotateBytes               string
+	AppRotateBytesHigh           string
+	AppRotateFiles               string
+	AppRotateOnline              string
+	AppRotateSeconds             string
+	AppStderr                    string
+	AppStderrCreationDisposition string
+	AppStderrFlagsAndAttributes  string
+	AppStderrShareMode           string
+	AppStdin                     string
+	AppStdinCreationDisposition  string
+	AppStdinFlagsAndAttributes   string
+	AppStdinShareMode            string
+	AppStdout                    string
+	AppStdoutCreationDisposition string
+	AppStdoutFlagsAndAttributes  string
+	AppStdoutShareMode           string
+	AppStopMethodConsole         string
+	AppStopMethodSkip            string
+	AppStopMethodThreads         string
+	AppStopMethodWindow          string
+	AppThrottle                  string
+	DependOnGroup                string
+	DependOnService              string
+	Description                  string
+	DisplayName                  string
+	ImagePath                    string
+	Name                         string
+	ObjectName                   string
+	Start                        string
+	Type                         string
+}
