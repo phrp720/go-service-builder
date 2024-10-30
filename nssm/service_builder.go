f3d101265a6fef20f067f5e4f67f2b99ade8a55f
phrp <filippospapadakis1@gmail.com>
2024-10-30 12:23:56 +0100

    minor changes

--- nssm/service_builder.go
+++ nssm/service_builder.go
@@ -12,212 +12,212 @@ func NewServiceBuilder() *ServiceBuilder {
 	}
 }
 func (b *ServiceBuilder) ServiceName(serviceName string) *ServiceBuilder {
-	b.service.serviceName = serviceName
+	b.service.ServiceName = serviceName
 	return b
 }
 
 func (b *ServiceBuilder) AppAffinity(appAffinity string) *ServiceBuilder {
-	b.service.parameters.AppAffinity = appAffinity
+	b.service.Parameters.AppAffinity = appAffinity
 	return b
 }
 
 func (b *ServiceBuilder) AppDirectory(appDirectory string) *ServiceBuilder {
-	b.service.parameters.AppDirectory = appDirectory
+	b.service.Parameters.AppDirectory = appDirectory
 	return b
 }
 
 func (b *ServiceBuilder) AppEnvironment(appEnvironment string) *ServiceBuilder {
-	b.service.parameters.AppEnvironment = appEnvironment
+	b.service.Parameters.AppEnvironment = appEnvironment
 	return b
 }
 
 func (b *ServiceBuilder) AppEnvironmentExtra(appEnvironmentExtra string) *ServiceBuilder {
-	b.service.parameters.AppEnvironmentExtra = appEnvironmentExtra
+	b.service.Parameters.AppEnvironmentExtra = appEnvironmentExtra
 	return b
 }
 
 func (b *ServiceBuilder) AppExit(appExit string) *ServiceBuilder {
-	b.service.parameters.AppExit = appExit
+	b.service.Parameters.AppExit = appExit
 	return b
 }
 
 func (b *ServiceBuilder) Application(application string) *ServiceBuilder {
-	b.service.parameters.Application = application
+	b.service.Parameters.Application = application
 	return b
 }
 
 func (b *ServiceBuilder) AppNoConsole(appNoConsole string) *ServiceBuilder {
-	b.service.parameters.AppNoConsole = appNoConsole
+	b.service.Parameters.AppNoConsole = appNoConsole
 	return b
 }
 
 func (b *ServiceBuilder) AppParameters(appParameters string) *ServiceBuilder {
-	b.service.parameters.AppParameters = appParameters
+	b.service.Parameters.AppParameters = appParameters
 	return b
 }
 
 func (b *ServiceBuilder) AppPriority(appPriority string) *ServiceBuilder {
-	b.service.parameters.AppPriority = appPriority
+	b.service.Parameters.AppPriority = appPriority
 	return b
 }
 
 func (b *ServiceBuilder) AppRestartDelay(appRestartDelay string) *ServiceBuilder {
-	b.service.parameters.AppRestartDelay = appRestartDelay
+	b.service.Parameters.AppRestartDelay = appRestartDelay
 	return b
 }
 
 func (b *ServiceBuilder) AppRotateBytes(appRotateBytes string) *ServiceBuilder {
-	b.service.parameters.AppRotateBytes = appRotateBytes
+	b.service.Parameters.AppRotateBytes = appRotateBytes
 	return b
 }
 
 func (b *ServiceBuilder) AppRotateBytesHigh(appRotateBytesHigh string) *ServiceBuilder {
-	b.service.parameters.AppRotateBytesHigh = appRotateBytesHigh
+	b.service.Parameters.AppRotateBytesHigh = appRotateBytesHigh
 	return b
 }
 
 func (b *ServiceBuilder) AppRotateFiles(appRotateFiles string) *ServiceBuilder {
-	b.service.parameters.AppRotateFiles = appRotateFiles
+	b.service.Parameters.AppRotateFiles = appRotateFiles
 	return b
 }
 
 func (b *ServiceBuilder) AppRotateOnline(appRotateOnline string) *ServiceBuilder {
-	b.service.parameters.AppRotateOnline = appRotateOnline
+	b.service.Parameters.AppRotateOnline = appRotateOnline
 	return b
 }
 
 func (b *ServiceBuilder) AppRotateSeconds(appRotateSeconds string) *ServiceBuilder {
-	b.service.parameters.AppRotateSeconds = appRotateSeconds
+	b.service.Parameters.AppRotateSeconds = appRotateSeconds
 	return b
 }
 
 func (b *ServiceBuilder) AppStderr(appStderr string) *ServiceBuilder {
-	b.service.parameters.AppStderr = appStderr
+	b.service.Parameters.AppStderr = appStderr
 	return b
 }
 
 func (b *ServiceBuilder) AppStderrCreationDisposition(appStderrCreationDisposition string) *ServiceBuilder {
-	b.service.parameters.AppStderrCreationDisposition = appStderrCreationDisposition
+	b.service.Parameters.AppStderrCreationDisposition = appStderrCreationDisposition
 	return b
 }
 
 func (b *ServiceBuilder) AppStderrFlagsAndAttributes(appStderrFlagsAndAttributes string) *ServiceBuilder {
-	b.service.parameters.AppStderrFlagsAndAttributes = appStderrFlagsAndAttributes
+	b.service.Parameters.AppStderrFlagsAndAttributes = appStderrFlagsAndAttributes
 	return b
 }
 
 func (b *ServiceBuilder) AppStderrShareMode(appStderrShareMode string) *ServiceBuilder {
-	b.service.parameters.AppStderrShareMode = appStderrShareMode
+	b.service.Parameters.AppStderrShareMode = appStderrShareMode
 	return b
 }
 
 func (b *ServiceBuilder) AppStdin(appStdin string) *ServiceBuilder {
-	b.service.parameters.AppStdin = appStdin
+	b.service.Parameters.AppStdin = appStdin
 	return b
 }
 
 func (b *ServiceBuilder) AppStdinCreationDisposition(appStdinCreationDisposition string) *ServiceBuilder {
-	b.service.parameters.AppStdinCreationDisposition = appStdinCreationDisposition
+	b.service.Parameters.AppStdinCreationDisposition = appStdinCreationDisposition
 	return b
 }
 
 func (b *ServiceBuilder) AppStdinFlagsAndAttributes(appStdinFlagsAndAttributes string) *ServiceBuilder {
-	b.service.parameters.AppStdinFlagsAndAttributes = appStdinFlagsAndAttributes
+	b.service.Parameters.AppStdinFlagsAndAttributes = appStdinFlagsAndAttributes
 	return b
 }
 
 func (b *ServiceBuilder) AppStdinShareMode(appStdinShareMode string) *ServiceBuilder {
-	b.service.parameters.AppStdinShareMode = appStdinShareMode
+	b.service.Parameters.AppStdinShareMode = appStdinShareMode
 	return b
 }
 
 func (b *ServiceBuilder) AppStdout(appStdout string) *ServiceBuilder {
-	b.service.parameters.AppStdout = appStdout
+	b.service.Parameters.AppStdout = appStdout
 	return b
 }
 
 func (b *ServiceBuilder) AppStdoutCreationDisposition(appStdoutCreationDisposition string) *ServiceBuilder {
-	b.service.parameters.AppStdoutCreationDisposition = appStdoutCreationDisposition
+	b.service.Parameters.AppStdoutCreationDisposition = appStdoutCreationDisposition
 	return b
 }
 
 func (b *ServiceBuilder) AppStdoutFlagsAndAttributes(appStdoutFlagsAndAttributes string) *ServiceBuilder {
-	b.service.parameters.AppStdoutFlagsAndAttributes = appStdoutFlagsAndAttributes
+	b.service.Parameters.AppStdoutFlagsAndAttributes = appStdoutFlagsAndAttributes
 	return b
 }
 
 func (b *ServiceBuilder) AppStdoutShareMode(appStdoutShareMode string) *ServiceBuilder {
-	b.service.parameters.AppStdoutShareMode = appStdoutShareMode
+	b.service.Parameters.AppStdoutShareMode = appStdoutShareMode
 	return b
 }
 
 func (b *ServiceBuilder) AppStopMethodConsole(appStopMethodConsole string) *ServiceBuilder {
-	b.service.parameters.AppStopMethodConsole = appStopMethodConsole
+	b.service.Parameters.AppStopMethodConsole = appStopMethodConsole
 	return b
 }
 
 func (b *ServiceBuilder) AppStopMethodSkip(appStopMethodSkip string) *ServiceBuilder {
-	b.service.parameters.AppStopMethodSkip = appStopMethodSkip
+	b.service.Parameters.AppStopMethodSkip = appStopMethodSkip
 	return b
 }
 
 func (b *ServiceBuilder) AppStopMethodThreads(appStopMethodThreads string) *ServiceBuilder {
-	b.service.parameters.AppStopMethodThreads = appStopMethodThreads
+	b.service.Parameters.AppStopMethodThreads = appStopMethodThreads
 	return b
 }
 
 func (b *ServiceBuilder) AppStopMethodWindow(appStopMethodWindow string) *ServiceBuilder {
-	b.service.parameters.AppStopMethodWindow = appStopMethodWindow
+	b.service.Parameters.AppStopMethodWindow = appStopMethodWindow
 	return b
 }
 
 func (b *ServiceBuilder) AppThrottle(appThrottle string) *ServiceBuilder {
-	b.service.parameters.AppThrottle = appThrottle
+	b.service.Parameters.AppThrottle = appThrottle
 	return b
 }
 
 func (b *ServiceBuilder) DependOnGroup(dependOnGroup string) *ServiceBuilder {
-	b.service.parameters.DependOnGroup = dependOnGroup
+	b.service.Parameters.DependOnGroup = dependOnGroup
 	return b
 }
 
 func (b *ServiceBuilder) DependOnService(dependOnService string) *ServiceBuilder {
-	b.service.parameters.DependOnService = dependOnService
+	b.service.Parameters.DependOnService = dependOnService
 	return b
 }
 
 func (b *ServiceBuilder) Description(description string) *ServiceBuilder {
-	b.service.parameters.Description = description
+	b.service.Parameters.Description = description
 	return b
 }
 
 func (b *ServiceBuilder) DisplayName(displayName string) *ServiceBuilder {
-	b.service.parameters.DisplayName = displayName
+	b.service.Parameters.DisplayName = displayName
 	return b
 }
 
 func (b *ServiceBuilder) ImagePath(imagePath string) *ServiceBuilder {
-	b.service.parameters.ImagePath = imagePath
+	b.service.Parameters.ImagePath = imagePath
 	return b
 }
 
 func (b *ServiceBuilder) Name(name string) *ServiceBuilder {
-	b.service.parameters.Name = name
+	b.service.Parameters.Name = name
 	return b
 }
 
 func (b *ServiceBuilder) ObjectName(objectName string) *ServiceBuilder {
-	b.service.parameters.ObjectName = objectName
+	b.service.Parameters.ObjectName = objectName
 	return b
 }
 
 func (b *ServiceBuilder) Start(start string) *ServiceBuilder {
-	b.service.parameters.Start = start
+	b.service.Parameters.Start = start
 	return b
 }
 
 func (b *ServiceBuilder) Type(serviceType string) *ServiceBuilder {
-	b.service.parameters.Type = serviceType
+	b.service.Parameters.Type = serviceType
 	return b
 }
 
